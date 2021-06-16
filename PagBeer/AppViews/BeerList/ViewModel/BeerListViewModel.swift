9151aa17553ee57a045c73dfa50500d78f4517b0
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-06-15 23:00:34 -0300

    Update BeerListViewModel.swift
    Implementa protocolo UIBeerList (não acho o melhor nome, mas foi o que eu pensei haha) que exige uma função updateData que irá depender da ViewController implementar conforme seu desejo, minha parte de mandar os dados pra ela eu to fazendo *isso foi o BeerListViewModel falando*

--- PagBeer/AppViews/BeerList/ViewModel/BeerListViewModel.swift
+++ PagBeer/AppViews/BeerList/ViewModel/BeerListViewModel.swift
@@ -11,15 +11,22 @@ protocol BeerListViewModelDelegate {
     func beerDetails(model: BeerModel)
 }
 
+protocol UIBeerList {
+    func updateData(data: [BeerModel])
+}
+
 class BeerListViewModel {
     var delegate: BeerListViewModelDelegate?
+    var viewController: UIBeerList?
     
     func goToBeerDetails(model: BeerModel) {
         delegate?.beerDetails(model: model)
     }
     
-    func getBeerList() -> [BeerModel] {
-        return BeerListService().getBeerList()
+    func getBeerList() {
+        BeerListService().fetchBeers { (beers) in
+            self.viewController?.updateData(data: beers)
+        }
     }
 }
 
